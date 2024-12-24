60a2c4142add049c2a00f5dd2bd5b9c0cd80d4a6
khai-pi <khaiphamhoang.pi@gmail.com>
2024-12-24 13:57:38 +0100

    add login function

--- src/lib/services/auth/login/login.ts
+++ src/lib/services/auth/login/login.ts
@@ -1,1 +1,50 @@
-// TODO
+import { Session } from "@prisma/client";
+import { compare } from "bcryptjs";
+
+import { User } from "@/types/user";
+import { prisma } from "@/lib/prisma";
+
+import {
+  createSession,
+  generateSessionToken,
+  setSessionTokenCookie,
+} from "../session/session";
+
+interface LoginResponse {
+  token: string;
+  user: User;
+  session: Session;
+}
+
+export async function login(
+  email: string,
+  password: string
+): Promise<LoginResponse> {
+  try {
+    // const hashedPassword = await hash(password, 12);
+
+    const user = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!(await compare(password, user.password))) {
+      throw new Error("Invalid password");
+    }
+
+    const token = generateSessionToken();
+
+    const session = await createSession(token, user.id);
+
+    setSessionTokenCookie(token, session.expiresAt);
+
+    return { token, user, session };
+  } catch (error) {
+    throw new Error(error instanceof Error ? error.message : "Unknown error");
+  }
+}
