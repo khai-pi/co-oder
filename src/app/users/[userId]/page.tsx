8189ed05e69d1363b53e9cc81bd4a5f610704bd0
khai-pi <khaiphamhoang.pi@gmail.com>
2024-12-29 11:19:06 +0100

    style user page

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -19,10 +19,12 @@ export default async function UserPage({ params }: { params: Params }) {
   }
 
   return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-      {projects.map((project) => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
+    <div className="container mx-auto px-4 py-8">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 }
